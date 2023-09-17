c568ff11df4817314de51ebac92137aa9a6d2e24
hyunho4621 <137243882+hyunho4621@users.noreply.github.com>
2023-09-17 21:42:55 +0900

    2023-09-17 다음 주소 API 파싱

---
+++ lib/const/bottom_image_item.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+const iconList = [
+  Icons.home,
+  Icons.search,
+  Icons.account_circle,
+  Icons.production_quantity_limits,
+];
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:customer_manager/const/bottom_image_item.dart';
+import 'package:customer_manager/widgets/home_screen.dart';
+import 'package:customer_manager/widgets/profile_screen.dart';
+import 'package:customer_manager/widgets/search_screen.dart';
+import 'package:customer_manager/widgets/shopping_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainPage());
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _bottomNavIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold (
+        body: moviePage(),
+
+        floatingActionButton: FloatingActionButton (
+          onPressed: () {
+
+          },
+          child: const Icon(Icons.dark_mode),
+        ),
+
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+        bottomNavigationBar: AnimatedBottomNavigationBar (
+          icons: iconList,
+          activeIndex: _bottomNavIndex,
+          gapLocation: GapLocation.center,
+          notchSmoothness: NotchSmoothness.verySmoothEdge,
+          leftCornerRadius: 32,
+          rightCornerRadius: 32,
+          onTap: (index) => setState(() => _bottomNavIndex = index),
+        ),
+      ),
+    );
+  }
+
+  Widget moviePage() {
+    switch(_bottomNavIndex) {
+      case 0:
+        return const HomeScreen();
+      case 1:
+        return const SearchScreen();
+      case 2:
+        return const ProfileScreen();
+      case 3:
+        return const ShoppingScreen();
+      default:
+        return const MainPage();
+    }
+  }
+}
