377961d6cf33a6ae94691898225a7138b65f733e
hyunho4532 <kyounguk192@gmail.com>
2023-10-18 22:44:21 +0900

    2023-10-18 회원가입 첫 번째 페이지 구현 완료

--- lib/widgets/register/update_register_screen.dart
+++ lib/widgets/register/update_register_screen.dart
@@ -36,11 +36,15 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
 
   final PageController _pageController = PageController(initialPage: 0);
   final TextEditingController _emailTextController = TextEditingController();
+  final TextEditingController _passwordTextController = TextEditingController();
   final ValueNotifier<bool> _isEmailValid = ValueNotifier<bool>(false);
+  final ValueNotifier<bool> _isPasswordValid = ValueNotifier<bool>(false);
 
   bool isShowBodyText = false;
   bool isShowSectionText = false;
   bool isShowEmailText = false;
+  bool isShowPasswordText = false;
+  bool isShowNextButton = false;
 
   @override
   void initState() {
@@ -48,10 +52,15 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
 
     _showBodyTextDelayed();
     _showBodySectionDelayed();
-    _showBodyEmailTextDelayed();
-    
+    _showBodyEmailAndPasswordTextDelayed();
+    _showBodyNextButtonDelayed();
+
     _emailTextController.addListener(() {
       _isEmailValid.value = _emailTextController.text.isNotEmpty;
+    });
+
+    _passwordTextController.addListener(() {
+      _isPasswordValid.value = _passwordTextController.text.isNotEmpty;
     });
   }
 
@@ -66,7 +75,7 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
           child: Stack(
             children: [
               Positioned (
-                top: 300,
+                top: 420,
                 left: 0,
                 right: 0,
                 bottom: 0,
@@ -94,7 +103,7 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
                         padding: const EdgeInsets.only(top: 60.0, left: 24.0, right: 24.0),
                         child: MaterialTextField (
                           keyboardType: TextInputType.emailAddress,
-                          hint: 'Email',
+                          hint: '이메일을 입력해주세요',
                           labelText: 'Email',
                           textInputAction: TextInputAction.next,
                           prefixIcon: const Icon(Icons.email_outlined),
@@ -103,43 +112,66 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
                         ),
                       ),
 
-                    Padding (
-                      padding: const EdgeInsets.only(top: 24.0, left: 24.0),
-                      child: ValueListenableBuilder(
-                        valueListenable: _isEmailValid,
-                        builder: (context, isValid, child) {
-                          return PrettyButton(
-                            isDisable: false,
-                            blurRadius: 5,
-                            spreadRadius: 1,
-                            topShadowColor: Theme.of(context).colorScheme.onPrimary,
-                            bottomShadowColor: Theme.of(context).colorScheme.primary,
-                            backgroundColor: Theme.of(context).colorScheme.outlineVariant,
-                            width: 60,
-                            height: 40,
-                            borderRadius: 15,
-                            innerPadding: const EdgeInsets.all(8.0),
-                            outerPadding: const EdgeInsets.all(8.0),
-                            onTap: () {
-                              isValid ?
-                                _pageController.animateToPage(
-                                    1,
-                                    duration: const Duration(seconds: 2),
-                                    curve: Curves.easeInOut
-                                )
-                                  : null;
-                            },
-                            child: Text('다음',
-                              style: TextStyle(
-                                color: Theme.of(context).colorScheme.primary,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          );
-                        },
+                    if (isShowPasswordText)
+                      Padding (
+                        padding: const EdgeInsets.only(top: 30.0, left: 24.0, right: 24.0),
+                        child: MaterialTextField (
+                          keyboardType: TextInputType.emailAddress,
+                          hint: '패스워드를 입력해주세요',
+                          labelText: 'Password',
+                          textInputAction: TextInputAction.next,
+                          prefixIcon: const Icon(Icons.password_outlined),
+                          controller: _passwordTextController,
+                          validator: FormValidation.emailTextField,
+                        ),
                       ),
-                    )
+
+
+                    if (isShowNextButton)
+
+                      Padding (
+                        padding: const EdgeInsets.only(top: 24.0, left: 24.0),
+                        child: ValueListenableBuilder<bool>(
+                          valueListenable: _isEmailValid,
+                          builder: (context, isEmailValid, child) {
+                            return ValueListenableBuilder<bool>(
+                              valueListenable: _isPasswordValid,
+                              builder: (context, isPasswordValid, child) {
+                                return PrettyButton (
+                              
+                                  isDisable: false,
+                                  blurRadius: 5,
+                                  spreadRadius: 1,
+                                  topShadowColor: Theme.of(context).colorScheme.onPrimary,
+                                  bottomShadowColor: Theme.of(context).colorScheme.primary,
+                                  backgroundColor: Theme.of(context).colorScheme.outlineVariant,
+                                  width: 60,
+                                  height: 50,
+                                  borderRadius: 15,
+                                  innerPadding: const EdgeInsets.all(8.0),
+                                  outerPadding: const EdgeInsets.all(8.0),
+                                  onTap: () {
+                                    _isEmailValid.value && _isPasswordValid.value ?
+                                    _pageController.animateToPage(
+                                        1,
+                                        duration: const Duration(seconds: 2),
+                                        curve: Curves.easeInOut
+                                    )
+                                        : null;
+                                    },
+                                    child: Text('다음',
+                                      style: TextStyle(
+                                        color: Theme.of(context).colorScheme.primary,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      )
                   ],
                 ),
               ),
@@ -148,7 +180,7 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
         ),
         Container(
           color: Colors.orangeAccent.withOpacity(0.5),
-          child: Center(
+          child: const Center(
             child: Text(
               '두 번째 페이지',
               style: TextStyle(fontSize: 50),
@@ -157,7 +189,7 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
         ),
         Container(
           color: Colors.cyanAccent.withOpacity(0.5),
-          child: Center(
+          child: const Center(
             child: Text(
               '세 번째 페이지',
               style: TextStyle(fontSize: 50),
@@ -185,11 +217,20 @@ class _PageViewRegisterWidgetState extends State<PageViewRegisterWidget> {
     });
   }
 
-  void _showBodyEmailTextDelayed() {
+  void _showBodyEmailAndPasswordTextDelayed() {
     Future.delayed(const Duration(seconds: 6), () {
       setState(() {
         isShowEmailText = true;
+        isShowPasswordText = true;
       });
+    });
+  }
+
+  void _showBodyNextButtonDelayed() {
+    Future.delayed(const Duration(seconds: 8), () {
+       setState(() {
+         isShowNextButton = true;
+       });
     });
   }
 }
