ba9d757da108ea8f6ca1a69ec8be614185862f70
hyunho4532 <kyounguk192@gmail.com>
2023-11-12 22:42:38 +0900

    2023-11-12 카테고리 필드 추가 완료

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -67,183 +67,179 @@ class _HomeScreenState extends State<HomeScreen> {
                           );
                         },
 
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Row (
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                        child: SizedBox (
+                          width: 150,
+                          height: 150,
 
-                            children: [
-                              imageUrl != null
-                                  ? SizedBox(
-                                width: 100,
-                                height: 100,
-                                child: CachedNetworkImage (
-                                  imageUrl: '$imageUrl',
-                                  fit: BoxFit.cover,
-                                  placeholder: (context, url) => const CircularProgressIndicator(),
-                                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                          child: Padding (
+                            padding: const EdgeInsets.all(16.0),
+                            child: Row (
+                              crossAxisAlignment: CrossAxisAlignment.start,
+
+                              children: [
+                                imageUrl != null
+                                    ? SizedBox(
+                                  width: 105,
+                                  height: 110,
+                                  child: CachedNetworkImage (
+                                    imageUrl: '$imageUrl',
+                                    fit: BoxFit.cover,
+                                    placeholder: (context, url) => const CircularProgressIndicator(),
+                                    errorWidget: (context, url, error) => const Icon(Icons.error),
+                                  ),
+                                )
+                                    : Container(
+                                  width: 150,
+                                  height: 150,
+                                  color: Colors.grey,
                                 ),
-                              )
-                                  : Container(
-                                width: 150,
-                                height: 150,
-                                color: Colors.grey,
-                              ),
 
-                              const SizedBox(width: 16),
+                                const SizedBox(width: 16),
 
-                              Column (
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                                Column (
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
 
-                                children: [
-                                  Row (
-                                    children: [
-                                      Padding (
-                                        padding: const EdgeInsets.only(top: 6.0),
-                                        child: SizedBox (
-                                          width: 120,
-                                          height: 30,
+                                  children: [
+                                    Row (
+                                      children: [
+                                        Padding (
+                                          padding: const EdgeInsets.only(top: 6.0),
+                                          child: SizedBox (
+                                            width: 120,
+                                            height: 30,
 
-                                          child: Text (
-                                            product.title.length > 8
-                                                ? '${product.title.substring(0, 14)}...'
-                                                : product.title,
-                                            style: const TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 16.0,
+                                            child: Text (
+                                              product.title.length > 8
+                                                  ? '${product.title.substring(0, 14)}...'
+                                                  : product.title,
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 16.0,
+                                              ),
+                                              overflow: TextOverflow.ellipsis,
                                             ),
-                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+
+                                        Padding (
+                                          padding: const EdgeInsets.only(top: 6.0, bottom: 12.0, left: 60.0),
+                                          child: GestureDetector (
+                                            onTap: () {
+                                              setState(() {
+                                                isFavorite = !isFavorite;
+
+                                                AnimatedSnackBar.material(
+                                                  '관심 물품으로 등록하였습니다!',
+                                                  type: AnimatedSnackBarType.success,
+                                                ).show(context);
+
+                                                _productRepository.insertFavoriteProduct(auth.currentUser!.uid.toString(), product.title, product.address, imageUrl, product.price);
+                                              });
+                                            },
+
+                                            child: isFavorite ?
+                                            Image.asset(
+                                              'assets/image/lock_favorite.png',
+                                              width: 20,
+                                              height: 20,
+                                            )
+                                                : Image.asset(
+                                              'assets/image/unlock_favorite.png',
+                                              width: 20,
+                                              height: 20,
+                                            )
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+
+                                    SizedBox (
+                                      width: 120,
+
+                                      child: Padding (
+                                        padding: const EdgeInsets.only(bottom: 16.0),
+                                        child: Text (
+                                          product.userAddress,
+                                          style: const TextStyle (
+                                            fontSize: 14.0,
                                           ),
                                         ),
                                       ),
+                                    ),
 
-                                      Padding (
-                                        padding: const EdgeInsets.only(top: 6.0, bottom: 12.0, left: 60.0),
+                                    Row (
+                                      children: [
+                                        Text (
+                                          '${product.price} 원 ',
+                                          style: const TextStyle (
+                                            fontSize: 14.0,
+                                          ),
+                                        ),
+
+                                        Padding (
+                                            padding: const EdgeInsets.only(left: 80),
+                                            child: product.isChecked == true ? const Text (
+                                                '가격 제안 O'
+                                            ) : const Text (
+                                                '가격 제안 X'
+                                            )
+                                        ),
+                                      ],
+                                    ),
+
+                                    Padding (
+                                        padding: const EdgeInsets.only(left: 166.0, top: 8.0),
                                         child: GestureDetector (
                                           onTap: () {
-                                            setState(() {
-                                              isFavorite = !isFavorite;
-
-                                              AnimatedSnackBar.material(
-                                                '관심 물품으로 등록하였습니다!',
-                                                type: AnimatedSnackBarType.success,
-                                              ).show(context);
-
-                                              _productRepository.insertFavoriteProduct(auth.currentUser!.uid.toString(), product.title, product.address, imageUrl, product.price);
-                                            });
+                                            _productRepository.favoriteAddProducts();
                                           },
 
-                                          child: isFavorite ?
-                                          Image.asset(
-                                            'assets/image/lock_favorite.png',
-                                            width: 20,
-                                            height: 20,
-                                          )
-                                              : Image.asset(
-                                            'assets/image/unlock_favorite.png',
-                                            width: 20,
-                                            height: 20,
-                                          )
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-
-                                  SizedBox (
-                                    width: 120,
-
-                                    child: Padding (
-                                      padding: const EdgeInsets.only(bottom: 16.0),
-                                      child: Text (
-                                        product.address,
-                                        style: const TextStyle (
-                                          fontSize: 14.0,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-
-                                  Row (
-                                    children: [
-                                      Text (
-                                        '${product.price} 원 ',
-                                        style: const TextStyle (
-                                          fontSize: 14.0,
-                                        ),
-                                      ),
-
-                                      Padding (
-                                          padding: const EdgeInsets.only(left: 80),
-                                          child: product.isChecked == true ? const Text (
-                                              '가격 제안 O'
-                                          ) : const Text (
-                                              '가격 제안 X'
-                                          )
-                                      ),
-                                    ],
-                                  ),
-
-                                  Row (
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-
-                                    children: [
-
-                                      Padding (
-                                          padding: const EdgeInsets.only(left: 162.0),
-                                          child: GestureDetector (
-                                            onTap: () {
-                                              _productRepository.favoriteAddProducts();
-                                            },
-
-                                            child: Text (
-                                              '관심: ${product.favoriteCount.toString()}',
-                                              style: const TextStyle (
-                                                fontSize: 15.0,
-                                                fontWeight: FontWeight.bold,
-                                              ),
+                                          child: Text (
+                                            product.category,
+                                            style: const TextStyle (
+                                              fontSize: 15.0,
+                                              fontWeight: FontWeight.bold,
                                             ),
-                                          )
-                                      ),
-                                    ],
-                                  ),
+                                          ),
+                                        )
+                                    ),
 
-                                  SizedBox (
-                                    width: 0,
-                                    height: 0,
+                                    SizedBox (
+                                      width: 0,
+                                      height: 0,
 
-                                    child: Opacity (
-                                      opacity: 0.0,
+                                      child: Opacity (
+                                        opacity: 0.0,
 
-                                      child: Text (
-                                        product.userAddress,
-                                        style: const TextStyle (
-                                          fontSize: 14.0,
+                                        child: Text (
+                                          product.userAddress,
+                                          style: const TextStyle (
+                                            fontSize: 14.0,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
 
-                                  SizedBox (
-                                    width: 0,
-                                    height: 0,
+                                    SizedBox (
+                                      width: 0,
+                                      height: 0,
 
-                                    child: Opacity (
-                                      opacity: 0.0,
+                                      child: Opacity (
+                                        opacity: 0.0,
 
-                                      child: Text (
-                                        product.nickname,
-                                        style: const TextStyle (
-                                          fontSize: 14.0,
+                                        child: Text (
+                                          product.nickname,
+                                          style: const TextStyle (
+                                            fontSize: 14.0,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              ),
-
-                            ],
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       );
